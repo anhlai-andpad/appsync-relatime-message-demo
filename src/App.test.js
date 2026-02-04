708ca12cfe23687ba34232442c1a2fcdc476d2df
anhlai <anh.lai@andpad.co.jp>
2026-02-04 20:01:47 +0700

    Rename header
    Signed-off-by: anhlai <anh.lai@andpad.co.jp>

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders AppSync Message Tester', () => {
+test('renders AppSync Message DEMO', () => {
   render(<App />);
   const heading = screen.getByText(/AppSync Message Tester/i);
   expect(heading).toBeInTheDocument();
